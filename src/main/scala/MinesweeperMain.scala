0cf9c4ce72142213c25e3e426ba516e372ed15c0
sebastian.thuemmel <sebastian.thuemmel@gmx.de>
2017-10-17 18:51:59 +0200

    removed unnecessary code

--- src/main/scala/MinesweeperMain.scala
+++ src/main/scala/MinesweeperMain.scala
@@ -1,11 +1,8 @@
 
 import de.htwg.mps.minesweeper.model.impl.Grid
 
-import Array._
-
 object MinesweeperMain {
 
-  //val playground: Array[Array[Field]] = ofDim[Field](10,10)
   val grid = new Grid(3,10,8)
 
   def main(args: Array[String])
