54e2eeb0d08b7070a86697db9e3e2079753fed54
Simon Keller <simonkeller94@aol.com>
2017-11-12 15:53:59 +0100

    Create a first test.

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "Scala_Minesweeper"
 version := "0.1"
 
 scalaVersion := "2.12.3"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
---
+++ src/test/scala/de/htwg/mps/minesweeper/model/impl/BombFieldTest.scala
@@ -0,0 +1,21 @@
+package de.htwg.mps.minesweeper.model.impl
+
+import org.scalatest.WordSpec
+
+class BombFieldTest extends WordSpec {
+
+  "A BombField" should {
+
+    val bombField = new BombField()
+
+    "be a bomb" in {
+      assert(bombField.isBomb)
+    }
+
+    "print a +" in {
+      assert(bombField.toString == "+")
+    }
+
+  }
+
+}
