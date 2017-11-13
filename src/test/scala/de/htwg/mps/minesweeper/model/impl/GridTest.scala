5b2a858218fd4b5b095af99ff5d2abf5865f0a12
Simon Keller <simonkeller94@aol.com>
2017-11-13 18:03:11 +0100

    Fix grid test.

--- src/test/scala/de/htwg/mps/minesweeper/model/impl/GridTest.scala
+++ src/test/scala/de/htwg/mps/minesweeper/model/impl/GridTest.scala
@@ -3,48 +3,41 @@ package de.htwg.mps.minesweeper.model.impl
 import org.scalatest.WordSpec
 
 class GridTest extends WordSpec {
-/*
+
   "A 10x10 Grid" should {
 
-    val grid_10 = new Grid(10)
+    val grid_10 = new Grid(10, 10, 18)
     grid_10.init()
 
     "have width" in {
-      assert(grid_10.width == 10)
+      assert(grid_10.playground.cols == 10)
     }
 
     "have height" in {
-      assert(grid_10.height == 10)
+      assert(grid_10.playground.rows == 10)
     }
 
     "have number of bombs" in {
       assert(grid_10.bombs == 18)
     }
 
-    "have a playground" in {
-      assert(grid_10.playground.length==10)
-    }
   }
 
   "A 5x7 Grid with 10 bombs" should {
-    val grid_5_7_10 = new Grid(5,7,10)
+    val grid_5_7_10 = new Grid(5, 7, 10)
     grid_5_7_10.init()
 
     "have width" in {
-      assert(grid_5_7_10.width == 5)
+      assert(grid_5_7_10.playground.rows == 5)
     }
 
     "have height" in {
-      assert(grid_5_7_10.height == 7)
+      assert(grid_5_7_10.playground.cols == 7)
     }
 
     "have number of bombs" in {
       assert(grid_5_7_10.bombs == 10)
     }
 
-    "have a playground" in {
-      assert(grid_5_7_10.playground.length==5)
-    }
   }
-*/
 }
