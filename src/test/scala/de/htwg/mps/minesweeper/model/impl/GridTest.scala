f708bc01d7def97228f982ff46a84215eebc1d82
Sebastian Thümmel <sethuemm@htwg-konstanz.de>
2017-11-13 09:42:01 +0100

    added additional Grid to GridTest

--- src/test/scala/de/htwg/mps/minesweeper/model/impl/GridTest.scala
+++ src/test/scala/de/htwg/mps/minesweeper/model/impl/GridTest.scala
@@ -6,22 +6,42 @@ class GridTest extends WordSpec {
 
   "A Grid" should {
 
-    val grid = new Grid(10)
+    val grid_10 = new Grid(10)
 
     "have width" in {
-      assert(grid.width == 10)
+      assert(grid_10.width == 10)
     }
 
     "have height" in {
-      assert(grid.height == 10)
+      assert(grid_10.height == 10)
     }
 
     "have number of bombs" in {
-      assert(grid.bombs == 18)
+      assert(grid_10.bombs == 18)
     }
 
     "have a playground" in {
-      assert(grid.playground.length==10)
+      assert(grid_10.playground.length==10)
+    }
+  }
+
+  "B Grid" should {
+    val grid_5_7_10 = new Grid(5,7,10)
+
+    "have width" in {
+      assert(grid_5_7_10.width == 5)
+    }
+
+    "have height" in {
+      assert(grid_5_7_10.height == 7)
+    }
+
+    "have number of bombs" in {
+      assert(grid_5_7_10.bombs == 10)
+    }
+
+    "have a playground" in {
+      assert(grid_5_7_10.playground.length==5)
     }
   }
 
