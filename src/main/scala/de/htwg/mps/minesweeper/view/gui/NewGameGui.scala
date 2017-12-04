adbfdc068ff885965aa1c35f04abc967a0871a16
Simon Keller <simonkeller94@aol.com>
2017-12-04 14:00:08 +0100

    Better layout for new game window.

--- src/main/scala/de/htwg/mps/minesweeper/view/gui/NewGameGui.scala
+++ src/main/scala/de/htwg/mps/minesweeper/view/gui/NewGameGui.scala
@@ -1,30 +1,23 @@
 package de.htwg.mps.minesweeper.view.gui
 
+import javax.swing.border.EmptyBorder
+
 import de.htwg.mps.minesweeper.controller.GameController
 
-import scala.swing.{Action, BorderPanel, Button, Frame, Label, TextField}
+import scala.swing.{Action, BorderPanel, BoxPanel, Button, Font, Frame, Label, Orientation, TextField}
 
 class NewGameGui(controller: GameController) extends Frame {
 
   title = "Start new game"
 
+  val headingFont = new Font("Arial", 0, 20)
+
   contents = new BorderPanel() {
+    add(heading, BorderPanel.Position.North)
+    add(textFields, BorderPanel.Position.Center)
+    add(buttons, BorderPanel.Position.South)
 
-    private def labelsPanel: BorderPanel = new BorderPanel() {
-      val labelRows: Label = new Label {
-        text = "Reihen"
-      }
-      val labelCols: Label = new Label {
-        text = "Spalten"
-      }
-      val labelBombs: Label = new Label {
-        text = "Minen"
-      }
-
-      add(labelRows, BorderPanel.Position.North)
-      add(labelCols, BorderPanel.Position.Center)
-      add(labelBombs, BorderPanel.Position.South)
-    }
+    border = new EmptyBorder(10, 10, 10, 10)
 
     private def restartButton: Button = new Button {
       text = "restart"
@@ -34,27 +27,59 @@ class NewGameGui(controller: GameController) extends Frame {
       }
     }
 
-    val textFieldRows: TextField = new TextField {
+    private lazy val textFieldRows: TextField = new TextField {
       text = controller.game.grid().getSize._1.toString
+      columns = 10
     }
-    val textFieldCols: TextField = new TextField {
+    private lazy val textFieldCols: TextField = new TextField {
       text = controller.game.grid().getSize._2.toString
+      columns = 10
     }
-    val textFieldBombs: TextField = new TextField {
+    private lazy val textFieldBombs: TextField = new TextField {
       text = controller.game.grid().bombs.toString
+      columns = 10
     }
 
-    private def textFieldPanel: BorderPanel = new BorderPanel() {
-      add(textFieldCols, BorderPanel.Position.North)
-      add(textFieldRows, BorderPanel.Position.Center)
-      add(textFieldBombs, BorderPanel.Position.South)
+    private def heading = new BorderPanel() {
+      add(new Label {
+        text = "Start a new minesweeper game"
+        font = headingFont
+      }, BorderPanel.Position.West)
+
+      border = new EmptyBorder(0, 0, 10, 0)
     }
 
-    add(labelsPanel, BorderPanel.Position.West)
-    add(textFieldPanel, BorderPanel.Position.Center)
-    add(restartButton, BorderPanel.Position.East)
+    private def buttons = new BorderPanel() {
+      add(restartButton, BorderPanel.Position.East)
+    }
+
+    private def textFields = new BoxPanel(Orientation.Vertical) {
+      contents += new BorderPanel() {
+        add(inputBorderPanel("rows"), BorderPanel.Position.West)
+        add(textFieldRows, BorderPanel.Position.East)
+
+        border = new EmptyBorder(5, 0, 5, 0)
+      }
+
+      contents += new BorderPanel() {
+        add(inputBorderPanel("columns"), BorderPanel.Position.West)
+        add(textFieldCols, BorderPanel.Position.East)
+
+        border = new EmptyBorder(5, 0, 5, 0)
+      }
+
+      contents += new BorderPanel() {
+        add(inputBorderPanel("bombs"), BorderPanel.Position.West)
+        add(textFieldBombs, BorderPanel.Position.East)
+
+        border = new EmptyBorder(5, 0, 5, 0)
+      }
+
+      private def inputBorderPanel(textDesc: String) = new Label {
+        text = textDesc
+        border = new EmptyBorder(0, 0, 0, 10)
+      }
+    }
 
   }
-
-
 }
